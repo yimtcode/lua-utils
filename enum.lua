a470e3da7014a3dd5da3155d6f0841dbd44adbfb
yimt <2579881587@qq.com>
2020-12-11 11:29:28 +0800

    Add find in the enum.lua

--- enum.lua
+++ enum.lua
@@ -26,7 +26,7 @@ end
 ---
 ---@param source table @数据源
 ---@param funcCond function @过滤方法,funcCond(value)返回值true保留,false删除
----@return table
+---@return table @新的数据
 function enum.filter(source, funcCond)
     local new = {}
 
@@ -37,6 +37,22 @@ function enum.filter(source, funcCond)
     end
 
     return new
+end
+
+---
+--- 查找第一个符合条件元素
+---
+---@param source table @数据源
+---@param funcCond function @查询条件,如果funcCond(value)为true满条件,为false不满足条件
+---@return any @第一个满足条件的元素,如果没有满足条件返回nil
+function enum.find(source, funcCond)
+    for _, v in pairs(source) do
+        if funcCond(v) then
+            return v
+        end
+    end
+
+    return nil
 end
 
 return enum
